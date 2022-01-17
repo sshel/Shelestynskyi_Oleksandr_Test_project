6b568421e603b273500348c1ac0b7b2588ae9d5f
Shelestynskyi <sshel98@gmail.com>
2022-01-17 14:14:42 +0200

    Added user data file for UI testing

--- cypress/cypress/integration/UI-testing/testingUI.js
+++ cypress/cypress/integration/UI-testing/testingUI.js
@@ -13,13 +13,15 @@ describe('UI testing',()=>{
     })
 
     it('Submit a user form with 3-5 years experience, two expertise and post graduate education',()=>{
-        cy.get('#g2599-name').type('User')
-        cy.get('#g2599-email').type('user@gmail.com')
-        cy.get('#g2599-experienceinyears').select('3-5')
-        cy.get('[value="Functional Testing"]').check()
-        cy.get('[value="Manual Testing"]').check()
-        cy.get('[value="Post Graduate"]').check()
-        cy.get('[name="g2599-comment"]').type('Some text')
+        cy.fixture('dataUI.json').then(dataUI=>{
+            cy.get('#g2599-name').type(dataUI.name)
+            cy.get('#g2599-email').type(dataUI.email)
+            cy.get('#g2599-experienceinyears').select('3-5')
+            cy.get('[value="Functional Testing"]').check()
+            cy.get('[value="Manual Testing"]').check()
+            cy.get('[value="Post Graduate"]').check()
+            cy.get('[name="g2599-comment"]').type(dataUI.comment)
+        })
         cy.get('.pushbutton-wide').click()
         cy.contains('Expertise :: Functional Testing, Manual Testing').should('be.visible')
         cy.contains('Education: Post Graduate').should('be.visible')
@@ -29,16 +31,20 @@ describe('UI testing',()=>{
 
         const fileName='small_img.jpg'
         cy.get('input[type=file]').attachFile(fileName)
-        cy.get('#g2599-name').type('User')
-        cy.get('#g2599-email').type('user@gmail.com')
-        cy.get('[name="g2599-comment"]').type('Some text')
+        cy.fixture('dataUI.json').then(dataUI=>{
+            cy.get('#g2599-name').type(dataUI.name)
+            cy.get('#g2599-email').type(dataUI.email)
+            cy.get('[name="g2599-comment"]').type(dataUI.comment)
+        })
         cy.get('.pushbutton-wide').click()
         cy.contains('Name: User').should('be.visible')
     })
 
     it('Submit a user form with empty comment field', () => {
-        cy.get('#g2599-name').type('User')
-        cy.get('#g2599-email').type('user@gmail.com')
+        cy.fixture('dataUI.json').then(dataUI=>{
+            cy.get('#g2599-name').type(dataUI.name)
+            cy.get('#g2599-email').type(dataUI.email)
+        })
         cy.get('button[type="submit"]').click()
         cy.get('[name="g2599-comment"]').then(($input) => {
              expect($input[0].validationMessage).to.eq('Please fill out this field.')
